0d397046bb75ed43bf926e3de694ffb179ded54e
KieranFowler <kieran9fowler@gmail.com>
2024-11-26 15:50:09 +0000

    create default layout file, import Link and Outlet include header, nav with Links to home and create post

---
+++ src/layout/default.jsx
@@ -0,0 +1,32 @@
+//import { logo } from "../assets";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+const Default = () => {
+  return (
+    <>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py 4 border-b border-b- [#e6ebf4]">
+        {/* {<Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-cotain" />
+        </Link>} */}
+
+        <nav>
+          <Link to="/" className="w-28 object-contain">
+            Home
+          </Link>
+          <Link
+            to="/create-post"
+            className="font-iter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          >
+            Create
+          </Link>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Default;
