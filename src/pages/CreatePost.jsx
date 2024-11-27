d15deaa566527ae1fd9f92487389547efe04c0f9
KieranFowler <kieran9fowler@gmail.com>
2024-11-27 18:45:36 +0000

    include generatingImg, Loader condition and add css

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -12,8 +12,10 @@ const CreatePost = () => {
     prompt: " ",
     photo: false,
   });
-  const [generateingImg, setGeneratingImg] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const generateImage = () => {};
 
   const handleSubmit = () => {};
 
@@ -22,7 +24,7 @@ const CreatePost = () => {
   const handleSurpriseMe = () => {};
 
   return (
-    <section className="max-w-8xl mx-auto">
+    <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
@@ -48,27 +50,44 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurprise={handleSurpriseMe}
           />
-        
-        <div
-          className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+
+          <div
+            className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
       focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center"
-        >
-          
-          {form.photo ? (
-            <img
-              src={form.photo}
-              alt={form.prompt}
-              className="w-full h-full object-contain"
-            />
-          ) : (
-            <img
-              src={preview}
-              alt="preview"
-              className="w-9/12 h-9/12 object-contain opacity-40"
-            />
-          )}
+          >
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div
+                className="absolute inset-0 z-0 flex
+                justify-center items-center bg-[rgb(0,0,0,0.5)] rounded-lg"
+              >
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+        <div className="mt-5 flex gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className="text-white br-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5"
+              >
+                {generatingImg ? 'Generating...' : 'Generate'}
+              </button>
+        </div>
       </form>
     </section>
   );
