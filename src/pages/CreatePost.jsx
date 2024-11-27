8f07998030d3e9a3bfecb8cfade1491223086d25
KieranFowler <kieran9fowler@gmail.com>
2024-11-27 10:39:32 +0000

    create form usestate pass obj as default state. Add loading and generatingImg state. Include Formfield component and pass field properties

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,13 +1,54 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+
 import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
-import {Formfield, Loader} from '../components'
+import {FormField, Formfield, Loader} from '../components'
 
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: " ",
+    prompt: " ",
+    photo: " ",
+});
+const [generateingImg, setGeneratingImg] = useState(false);
+const [loading, setLoading] = useState(false);
+
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7 mx-auto'>
+      <div>
+      <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Creeate a imaginative image and share with the Community.
+        </p>
+        </div>
+        <form className='mt-16 max-w-3x1' onSubmit={handleSubmit}>
+          <FormField
+            labelName='your name'
+            type ='text'
+            name= 'name'
+            placeholder='james'
+            value={form.name}
+            handleChange={handelChange}
+          />
+           <FormField
+            labelName='Prompt'
+            type ='text'
+            name= 'name'
+            placeholder='a pencil and watercolor drawing of a bright city in the future with flying cars'
+            value={form.prompt}
+            handleChange={handelChange}
+            isSurpriseMe
+            handleSurprise={handleSurpriseMe}
+          />
+
+        </form>
+    </section>
   )
 }
 
