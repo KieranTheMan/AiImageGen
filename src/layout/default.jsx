f545c0d6debd16836df12e6e86ffd1964459dd18
KieranFowler <kieran9fowler@gmail.com>
2024-11-27 13:57:58 +0000

    edit css border-b-[#e6ebf4]

--- src/layout/default.jsx
+++ src/layout/default.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Default = () => {
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py 4 border-b border-b- [#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py 4 border-b border-b-[#e6ebf4]">
         {/* {<Link to="/">
           <img src={logo} alt="logo" className="w-28 object-cotain" />
         </Link>} */}
