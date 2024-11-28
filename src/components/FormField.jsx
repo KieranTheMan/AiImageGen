c61094475f052c895164af603f5e1372d068144e
KieranFowler <kieran9fowler@gmail.com>
2024-11-28 16:21:47 +0000

    fixed click button function trigger error by adding () to the end of handleSurpriseMe to execute function

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -10,6 +10,7 @@ const FormField = ({
   isSurpriseMe,
   handleSurpriseMe
 }) => {
+console.log(placeholder)
   return (
     <div>
       <div className="block items-center gap-2 mb-2">
@@ -24,7 +25,10 @@ const FormField = ({
         {isSurpriseMe && (
           <button
             type="button"
-            onClick={handleSurpriseMe}
+            onClick={() => {
+              handleSurpriseMe()
+              console.log('button onclick clicked')
+            }}
             className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise Me
@@ -38,7 +42,7 @@ const FormField = ({
           value={value}
           onChange={handleChange}
           required
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none w-full p-8"
+          className="bg-gray-50 border border-gray-300 text-gray-950 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none w-full p-8"
         />
       </div>
     </div>
