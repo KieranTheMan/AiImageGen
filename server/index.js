42d530593d42f4bb32837360d37765b46b6d8bba
KieranFowler <kieran9fowler@gmail.com>
2024-12-03 09:45:22 +0000

    import postRoute and dalleRoute and create a routes using express app.use()

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from './mongodb/connect.js'
+import postRoute from './routes/postRoute.js';
+import dalleRoute from './routes/DalleRoute.js';
+
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/v1/post', postRoute);
+app.use('/api/v1/dalle', dalleRoute);
 
 app.get("/", async (req, res) => {
   res.send("Hello API");
