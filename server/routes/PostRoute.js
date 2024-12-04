575743471f7fd5affa2ca43017a9828c45eccd9b
KieranFowler <kieran9fowler@gmail.com>
2024-12-04 10:00:51 +0000

    include cloudinary.config keys, creat get and post  all posts route with cloudinary.uplaoder and newPost ver

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -1,12 +1,34 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import {v2 as cloudinary} from 'cloudinary';
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
-import Post from '../mongodb/models/post.js';
+import Post from "../mongodb/models/post.js";
 
-dotenv.config()
-
-
+dotenv.config();
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//get all posts
+
+router.route("/").get(async (req, res) => {});
+
+//create all posts
+
+router.route("/").post(async (req, res) => {
+  const { name, prompt, photo } = req.body;
+  const photoUrl = await cloudinary.uploader.upload(photo);
+
+  const newPost = await Post.create({
+    name,
+    prompt,
+    photo: photoUrl.url,
+  });
+  res.status(200).json({ success: true, data: newPost });
+});
 
 export default router;
