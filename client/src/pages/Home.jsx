5835d1e47c21e74e170fb0ed486a73c8f5785be1
KieranFowler <kieran9fowler@gmail.com>
2024-12-04 12:35:58 +0000

    import useEffect, add useEffect and include a fetchPost function that GETS all posts and sets the setAllposts state to results that are reversed() then finally setLoading state to false

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,33 @@
-import React, { useState, useEffects } from "react";
+import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8000/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   const RenderCards = ({ data, title }) => {
     data?.length > 0 ? (
