f8082ba2838b0496a6a90ace59057eeac67eef4a
KieranFowler <kieran9fowler@gmail.com>
2024-12-04 11:31:12 +0000

    add try catch block with Post.find() and respond status codes 200 and 500 to get all posts route

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -15,20 +15,32 @@ cloudinary.config({
 
 //get all posts
 
-router.route("/").get(async (req, res) => {});
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
 
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 //create all posts
 
 router.route("/").post(async (req, res) => {
-  const { name, prompt, photo } = req.body;
-  const photoUrl = await cloudinary.uploader.upload(photo);
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-  const newPost = await Post.create({
-    name,
-    prompt,
-    photo: photoUrl.url,
-  });
-  res.status(200).json({ success: true, data: newPost });
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
 });
 
 export default router;
