c23552178cb784ba51d938c12ff5aa97546621dc
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 20:04:54 +0000

    add darkmode Button component

---
+++ client/src/components/DarkmodeButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DarkmodeButton = ({toggleDarkMode, darkMode}) => {
+
+  return (
+    <button className="absoulute w-16 h-16 bottom-16 bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold outline-dashed outline-blue-700  "
+    onClick={()=>toggleDarkMode()}>
+    {darkMode ? 'LHT' : 'DRK'}
+    </button>
+  )
+}
+
+export default DarkmodeButton;
