d2156e389d5184927dba7c8ce8508db14f2e5dc9
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 22:25:09 +0000

    replace fetch url with aiimagegen onrender.com

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8000/api/v1/dalle", {
+        const response = await fetch("https://aiimagegen-571e.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
