b1cadee5909bee53901216d8da24944ce7121d61
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 19:44:49 +0000

    add logo to link, remove home link change colors of buttons and nav backgound

--- client/src/layout/default.jsx
+++ client/src/layout/default.jsx
@@ -1,29 +1,38 @@
-//import { logo } from "../assets";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { logo } from "../assets"
+
 const Default = () => {
+  
+ 
+
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py 4 border-b border-b-[#e6ebf4]">
-        {/* {<Link to="/">
+      <header className="w-full flex justify-between items-cente bg-gradient-to-r from-sky-500 to-indigo-500 sm:px-8 px-4 py 4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-cotain" />
-        </Link>} */}
-
-        <nav>
-          <Link to="/" className="w-28 object-contain">
-            Home
-          </Link>
+        </Link>
+          
+        <nav className=" flex gap-1 mt-10 h-10 drop-shadow-xl">
+          
           <Link
             to="/create-post"
-            className="font-iter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+            className="font-iter font-medium bg-[#373df0] text-white px-4 py-2 rounded-md"
           >
             Create
+          </Link>
+          <Link
+            to="/"
+            className="font-iter font-medium bg-[#8B38D7] text-white px-4 py-2 rounded-md"
+          >
+            Showcase
           </Link>
         </nav>
       </header>
 
-      <main className= 'sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className= 'sm:p-8 px-4 py-8 w-full bg-slate-800   min-h-[calc(100vh-73px)]'>
         <Outlet />
+       
       </main>
       <footer></footer>
     </>
