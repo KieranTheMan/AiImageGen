3a8f61608ba1410b6b25333c38aba51f21b9fc2d
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 22:44:34 +0000

    convert tailwindcss to export  es model

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,28 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      screens: {
-        xs: '480px',
-      },
-      fontFamily: {
-        inter: ['Inter var', 'sans-serif'],
-      },
-      boxShadow: {
-        card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
-        cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
-      },
-      // colors: {
-      //   accent: {
-      //     1: "hsl(var(--color-accent1) / <alpha-value>)",
-      //     2:  "hsl(var(--color-accent2) / <alpha-value>)",
-      //   },
-      //   bkg: "hsl(var(--color-bkg) / <alpha-value>)",
-      //   content: "hsl(var(--color-content) / <alpha-value>)",
-      // },
+export const content = ['./src/**/*.{js,jsx}'];
+export const theme = {
+  extend: {
+    screens: {
+      xs: '480px',
     },
+    fontFamily: {
+      inter: ['Inter var', 'sans-serif'],
+    },
+    boxShadow: {
+      card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
+      cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
+    },
+    // colors: {
+    //   accent: {
+    //     1: "hsl(var(--color-accent1) / <alpha-value>)",
+    //     2:  "hsl(var(--color-accent2) / <alpha-value>)",
+    //   },
+    //   bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+    //   content: "hsl(var(--color-content) / <alpha-value>)",
+    // },
   },
-  plugins: [],
-  darkMode: "class",
 };
+export const plugins = [];
+export const darkMode = "class";
