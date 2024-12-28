95cd727342f9ad444be0b2d4cfd5bc447ea290e3
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 19:47:18 +0000

    import tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
@@ -12,7 +13,16 @@ module.exports = {
         card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
         cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
       },
+      // colors: {
+      //   accent: {
+      //     1: "hsl(var(--color-accent1) / <alpha-value>)",
+      //     2:  "hsl(var(--color-accent2) / <alpha-value>)",
+      //   },
+      //   bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+      //   content: "hsl(var(--color-content) / <alpha-value>)",
+      // },
     },
   },
   plugins: [],
+  darkMode: "class",
 };
