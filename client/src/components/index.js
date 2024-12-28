261c76b881212ba03ce9dc07c490e2fe9e849f24
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 20:06:01 +0000

    add DarkmodeButton to index.js for easy export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import Card from "./Card";
 import FormField from "./FormField";
 import Loader from "./Loader";
+import DarkmodeButton from "./DarkmodeButton";
 
 export {
     Card,
     FormField,
-    Loader
+    Loader,
+    DarkmodeButton,
 }
