00f48ab6e4bacf725dfeaf0cc145f17722b27e62
KieranFowler <kieran9fowler@gmail.com>
2024-12-28 20:04:02 +0000

    add logo and title to index for quick exports

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,8 +1,11 @@
 import download from './download.png';
-//import logo from './logo.svg';
+import logo from './aiartgen.png';
 import preview from './preview.png';
+import title from './aiartgentitle.png'
 
 export {
   download,
   preview,
+  logo,
+  title
 };
