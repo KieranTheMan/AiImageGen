1f9a22c835e62d1b65a95a5562caca759c86539e
KieranFowler <kieran9fowler@gmail.com>
2025-01-22 12:10:02 +0000

    add corsOptions Access-Control-Allow-Origin to cors function

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from './mongodb/connect.js'
-import postRoute from './routes/PostRoute.js';
-import dalleRoute from './routes/DalleRoute.js';
+import connectDB from "./mongodb/connect.js";
+import postRoute from "./routes/PostRoute.js";
+import dalleRoute from "./routes/DalleRoute.js";
 
-
-dotenv.config();
+const corsOptions = {
+  origin: "https://coolartgen.onrender.com",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
 
 const app = express();
-app.use(cors());
+dotenv.config();
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/post', postRoute);
-app.use('/api/v1/dalle', dalleRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/dalle", dalleRoute);
 
 app.get("/", async (req, res) => {
   res.send("Hello API");
