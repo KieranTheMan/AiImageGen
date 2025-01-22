fc3b490e640901d0bb7669e30c27dd332f410d67
KieranFowler <kieran9fowler@gmail.com>
2025-01-22 12:32:51 +0000

    change handle submit endpoint to onrender url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -49,7 +49,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8000/api/v1/post", {
+        const response = await fetch("https://aiimagegen-571e.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
