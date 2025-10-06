aeffe500e8bd282152844293d18ad11b7b368e19
KieranFowler <kieran9fowler@gmail.com>
2025-10-06 12:03:30 +0100

    Remove Cloudinary configuration from PostRoute.js to streamline code and enhance maintainability.

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -7,11 +7,6 @@ import Post from "../mongodb/models/post.js";
 dotenv.config();
 const router = express.Router();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 //get all posts
 
 router.route("/").get(async (req, res) => {
