08ed90da661f0477d61fcc922d3e39271f3c9f1d
KieranFowler <kieran9fowler@gmail.com>
2025-10-14 14:08:05 +0100

    Add IAM role for ECS task execution in Terraform module
    - Introduced a new IAM role for ECS task execution with an assume role policy for ECS tasks.
    - Attached the AmazonECSTaskExecutionRolePolicy to the IAM role for necessary permissions.
    - Enhanced resource management and security for ECS tasks by defining the role in the Terraform module.

---
+++ terraform/modules/iam-role/main.tf
@@ -0,0 +1,26 @@
+# modules/iam-roles/main.tf
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.cluster_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
