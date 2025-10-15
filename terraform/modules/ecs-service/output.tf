edd1eb3204777c028e2ed5fcd9fa4c66df00bda1
KieranFowler <kieran9fowler@gmail.com>
2025-10-15 13:24:11 +0100

    Add outputs for ECS service in Terraform module
    - Introduced output variables for ECS service name, service ID, task definition ARN, and CloudWatch log group name.
    - Enhanced visibility and management of ECS service resources by providing essential outputs.

---
+++ terraform/modules/ecs-service/output.tf
@@ -0,0 +1,19 @@
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.main.name
+}
+
+output "service_id" {
+  description = "ID of the ECS service"
+  value       = aws_ecs_service.main.id
+}
+
+output "task_definition_arn" {
+  description = "ARN of the task definition"
+  value       = aws_ecs_task_definition.main.arn
+}
+
+output "log_group_name" {
+  description = "Name of the CloudWatch log group"
+  value       = aws_cloudwatch_log_group.main.name
+}
